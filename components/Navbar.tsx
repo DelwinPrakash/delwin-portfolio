5fd4f14247be1e8c1f8e020c7e87c3d70a4a0343
= <delwinprakash499@gmail.com>
2025-04-07 17:49:04 +0530

    particles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import HamburgerMenu from "./icons/HamburgerMenu";
-import Close from "./icons/Close";
-// import GooeyNav from "./GooeyNav/GooeyNav";
-
 export default function Navbar() {
-    const [showNavBar, setShowNavBar] = useState(true);
-    const [isOpen, setIsOpen] = useState(false);
-    const [lastScrollY, setLastScrollY] = useState(0);
-
-    const toggleMenu = () => setIsOpen(!isOpen);
     
-    // const controllNavBar = () => {
-    // if (window.scrollY > lastScrollY) {
-    //     setShowNavBar(false);
-    // } else {
-    //     setShowNavBar(true);
-    // }
-    // setLastScrollY(window.scrollY);
-    // };
-
-    // useEffect(() => {
-    // window.addEventListener("scroll", controllNavBar);
-    // return () => {
-    //     window.removeEventListener("scroll", controllNavBar);
-    // };
-    // }, [lastScrollY]);
-
     const navLinks = [
         { href: "#about-me", label: "About me" },
         { href: "#projects", label: "Projects" },
@@ -35,51 +9,16 @@ export default function Navbar() {
     ];
     
     return(
-        <nav className="h-screen">
-            <header className={`${showNavBar ? "translate-y-0" : "-translate-y-full"} space-mono-font sticky top-0 z-50 backdrop-blur-xl backdrop-brightness-75 h-10 sm:h-16 flex items-center justify-center transition-transform duration-500 ${isOpen ? "translate-y-0" : ""}`}>
-                <div className="flex items-center justify-end w-screen mx-4">
-                    <div className="hidden sm:flex items-center justify-center w-full sm:space-x-10">
-                        {navLinks.map(({ href, label }) => (
-                            <a key={href} href={href} className="hover:text-blue-400">
-                                {label}
-                            </a>
-                        ))}
-                        {/* <GooeyNav
-                            items={navLinks}
-                            animationTime={600}
-                            colors={[1, 2, 3, 1, 2, 3, 1, 4]}
-                            timeVariance={300}
-                        /> */}
-                    </div>
-              
-                    <button className={`sm:hidden transition-transform duration-500 ${isOpen ? "rotate-180" : "rotate-0"}`}
-                        onClick={toggleMenu}
-                    >
-                        {isOpen ? <Close /> : <HamburgerMenu />}
-                    </button>
-                </div>
-            </header>
-                
-            <div className={`sm:hidden bg-stone-900 pb-16 space-mono-font fixed h-full w-screen text-white p-4 backdrop-blur-xl backdrop-brightness-75 z-50 transition-transform duration-500 ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
-                <nav className="mx-1 flex flex-col items-center justify-center space-y-10 h-full">
+        <nav className={`space-mono-font sticky top-0 h-16 sm:h-20 flex items-center justify-center`}>
+            <div className="flex items-center w-screen mx-2">
+                <div className="flex items-center justify-center w-full space-x-12 sm:space-x-20">
                     {navLinks.map(({ href, label }) => (
-                        <a
-                            key={href}
-                            href={href}
-                            className="block rounded-lg py-3 px-2 hover:bg-gray-900"
-                        >
+                        <a key={href} href={href} className="hover:text-blue-400">
                             {label}
                         </a>
                     ))}
-                    {/* <GooeyNav
-                        items={navLinks}
-                        animationTime={600}
-                        colors={[1, 2, 3, 1, 2, 3, 1, 4]}
-                        timeVariance={300}
-                    /> */}
-                </nav>
+                </div>
             </div>
         </nav>
-    );    
+    );
 }
-                        
