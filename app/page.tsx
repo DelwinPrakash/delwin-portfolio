1d952dbccfb7f085c0a54d48c1eba9b34074879b
= <delwinprakash499@gmail.com>
2025-04-07 17:49:29 +0530

    nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
-import Navbar from "@/components/Navbar"
+"use client";
+
+import Navbar from "@/components/Navbar";
+import Particles from "@/components/Particles/Particles";
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar/>
-      {/* <h2>Hi, I'm Delwin Prakash, WEB DEVELOPER</h2> */}
-    </div>
-  );
+    return (
+        <div className="relative w-full h-dvh overflow-hidden">
+            <Navbar/>
+            <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+                <Particles
+                    particleColors={['#ffffff', '#ff0f11']}
+                    particleCount={200}
+                    particleSpread={10}
+                    speed={0.1}
+                    particleBaseSize={100}
+                    moveParticlesOnHover={true}
+                    alphaParticles={false}
+                    disableRotation={false}
+                />
+            </div>
+            <div className="absolute inset-0 flex items-center justify-center z-10 text-white pointer-events-none">
+                <h1 className="text-4xl font-bold">Hey, I'm Delwin Prakash<br/>Web Developer</h1>
+            </div>
+        </div>
+    );
 }
