52a62338deef172ef5c14201bd3791c940cb204d
= <delwinprakash499@gmail.com>
2025-04-14 19:08:08 +0530

    fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,15 +3,16 @@
 export default function Navbar() {
     
     const navLinks = [
-        { href: "#about-me", label: "About me" },
-        { href: "#projects", label: "Projects" },
-        { href: "#contact", label: "Contact" }
+        { href: "/", label: "Home" },
+        { href: "/about", label: "About me" },
+        { href: "/projects", label: "Projects" },
+        { href: "/contact", label: "Contact" }
     ];
     
     return(
         <nav className={`h-16 sm:h-20 flex items-center justify-center bg-transparent`}>
-            <div className="flex items-center w-screen mx-2">
-                <div className="flex items-center justify-center w-full space-x-12 sm:space-x-20 font-mono">
+            <div className="flex items-center w-screen">
+                <div className="flex items-center justify-around w-full space-x-5 text-center font-mono">
                     {navLinks.map(({ href, label }) => (
                         <a key={href} href={href} className="text-white hover:text-blue-400 transition-colors pointer-events-auto">
                             {label}
