491fca814d7242d5c35d5beb6f5bca235be31f20
DelwinPrakash <delwinprakash499@gmail.com>
2025-09-12 23:25:10 +0530

    layout change

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,65 +1,63 @@
 "use client"
 
 import Footer from "@/components/Footer";
+import Particles from "@/components/Particles/Particles";
 import Card from "@/components/ui/Card";
 
 export default function About(){
     return(
-        <div className="inset-0 flex mt-16 sm:mt-20 z-10 text-white pointer-events-none p-2 h-full flex-col">
-            <div
-                className="fixed min-h-full -z-10 overflow-hidden inset-0 bg-[linear-gradient(to_right,rgba(255,0,0,0.2)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,0,0,0.2)_1px,transparent_1px)] bg-[size:40px_40px] md:bg-[size:40px_40px]"
-                style={{
-                    animation: "gridMove 20s linear infinite",
-                    backgroundPosition: "0 0",
-                }}
-            />
-            <style jsx>{`
-                @keyframes gridMove {
-                    0% {
-                        background-position: 0 0;
-                    }
-                    100% {
-                        background-position: 40px 40px;
-                    }
-                }
-            `}</style>
-            
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 w-full place-items-center mb-4">
-                <Card
-                    imageUrl="/thumbnail/chatapp.png"
-                    title="ChatApp"
-                    stack={['React Native', 'Supabase']}
-                    githubUrl="https://github.com/delwinprakash/chatapp"
-                    liveUrl="https://drive.google.com/drive/folders/1SDkq3SCXQDbxpLhrU2QdRqZ3IKfrgzNK?usp=sharing"
-                    isLive={true}
-                />
-                <Card
-                    imageUrl="/thumbnail/portfolio.png"
-                    title="Portfolio Website"
-                    stack={['NextJS', 'Typescript', 'Tailwind']}
-                    githubUrl="https://github.com/delwinprakash/portfolio"
-                    liveUrl="https://delwin.vercel.app/"
-                    isLive={true}
-                />
-                <Card
-                    imageUrl="/thumbnail/ecleanse.png"
-                    title="E-Cleanse"
-                    stack={['React', 'NodeJS', 'ExpressJS', 'MongoDB', 'Tailwind']}
-                    githubUrl="https://github.com/DelwinPrakash/E-Cleanse"
-                    liveUrl=""
-                    isLive={false}
-                />
-                <Card
-                    imageUrl="/thumbnail/nativeTodo.png"
-                    title="Todo App"
-                    stack={['React Native', 'Expo', 'Tailwind']}
-                    githubUrl="https://github.com/DelwinPrakash/react-native-todo"
-                    liveUrl=""
-                    isLive={false}
+        <div className="relative w-full h-full overflow-hidden">
+            <div className="absolute inset-0">
+                <Particles
+                    particleColors={['#40ffaa', '#4079ff']}
+                    particleCount={200}
+                    particleSpread={10}
+                    speed={0.1}
+                    particleBaseSize={100}
+                    moveParticlesOnHover={true}
+                    alphaParticles={false}
+                    disableRotation={false}
                 />
             </div>
-
-            <Footer/>
-        </div>
+            <div className="inset-0 flex flex-col items-center justify-center z-10 text-white mt-16 sm:mt-20 px-2">
+                <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mb-4 px-3">
+                    <Card
+                        imageUrl="/thumbnail/chatapp.png"
+                        title="ChatApp"
+                        stack={['React Native', 'Supabase']}
+                        githubUrl="https://github.com/delwinprakash/chatapp"
+                        liveUrl="https://drive.google.com/drive/folders/1SDkq3SCXQDbxpLhrU2QdRqZ3IKfrgzNK?usp=sharing"
+                        isLive={true}
+                    />
+                    <Card
+                        imageUrl="/thumbnail/portfolio.png"
+                        title="Portfolio Website"
+                        stack={['NextJS', 'Typescript', 'Tailwind']}
+                        githubUrl="https://github.com/delwinprakash/portfolio"
+                        liveUrl="https://delwin.vercel.app/"
+                        isLive={true}
+                    />
+                    <Card
+                        imageUrl="/thumbnail/ecleanse.png"
+                        title="E-Cleanse"
+                        stack={['React', 'NodeJS', 'ExpressJS', 'MongoDB', 'Tailwind']}
+                        githubUrl="https://github.com/DelwinPrakash/E-Cleanse"
+                        liveUrl=""
+                        isLive={false}
+                    />
+                    <Card
+                        imageUrl="/thumbnail/nativeTodo.png"
+                        title="Todo App"
+                        stack={['React Native', 'Expo', 'Tailwind']}
+                        githubUrl="https://github.com/DelwinPrakash/react-native-todo"
+                        liveUrl=""
+                        isLive={false}
+                    />
+                </div>
+                <div className="w-full px-3">
+                    <Footer/>
+                </div>
+            </div>
+        </div >
     );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,8 @@ export default function Footer(){
         { href: "/projects", label: "Projects" },
     ];
     return(
-        <footer className="bg-neutral-80 border-1 rounded-2xl border-red-600 font-pixelify text-xl flex flex-col justify-end py-2">
-            <div className="bg-red-5 flex justify-between px-5 sm:px-10 pointer-events-auto">
+        <footer className="mb-4 border-1 rounded-2xl border-red-600 font-pixelify text-xl flex flex-col justify-end py-2">
+            <div className="flex justify-between px-5 sm:px-10 pointer-events-auto">
                 <ul className="space-y-5 flex flex-col">
                     {navLinks.map(({ href, label }) => (
                         <a key={href} href={href} className="text-white hover:text-blue-400 transition-colors">
